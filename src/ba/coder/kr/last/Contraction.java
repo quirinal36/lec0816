51838092c0cc5d617167c792dded1da225d953f6
Lee <turboguy36@gmail.com>
2018-09-13 13:49:09 +0900

    문제 각 자릿수의 합

---
+++ src/ba/coder/kr/last/Contraction.java
@@ -0,0 +1,24 @@
+package ba.coder.kr.last;
+
+public class Contraction {
+	
+	// 만약 inputNumber 가 123 으로 들어온다면
+	public static int sumEach(int inputNumber){
+		int tot=0;		// 합을 저장할 변수
+		
+		while(inputNumber!=0){
+			tot+=inputNumber%10; // 3 -> 2-> 1
+			inputNumber/=10;     // 123-> 12-> 1->0
+		}
+		
+		return tot;
+	}
+	
+	public static void main(String[] args) {
+		int number=123;
+		
+		int value=sumEach(number);
+		
+		System.out.printf("%d에 대한 각자리의 수 합: %d" ,number ,value);
+	}
+}
